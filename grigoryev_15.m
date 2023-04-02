72378009f20ce7a36a65a5c157a2bb12339849af
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-04-02 17:41:49 +0700

    Compact variables and functions in tasks 15&16

--- grigoryev_15.m
+++ grigoryev_15.m
@@ -1,21 +1,16 @@
 % ПЗ 15, Григорьев Юрий
 clc;
 
-syms t;
-syms f0(t);
+syms t; syms f0(t); syms f(t); syms alpha(t);
 f0(t) = t; % начальное приближение
-
-syms f(t);
 f(t) = t + atan(f0(t)) * (t^2 - 1)/2; % Ф
-
-syms alpha(t);
 alpha(t) = (t^2 - 1)/2; % коэффициент сжатия
-eps = 1e-2;
+epsilon = 1e-2;
 a = -1.5;
 b = 1.5;
 
-apriori = int16(round(log(eps * (1 - alpha(a)) / maximum(f0, f, a, b)) / log(alpha(a))));
-fprintf("alphaMax = %f, eps = %.e, apr = %d\n", alpha(a), eps, apriori);
+apriori = int16(round(log(epsilon * (1 - alpha(a)) / maximum(f0, f, a, b)) / log(alpha(a))));
+fprintf("alphaMax = %f, eps = %.e, apr = %d\n", alpha(a), epsilon, apriori);
 
 % итерации
 i = 0;
@@ -31,10 +26,10 @@ plot(a:b, f(a:b));
 title('Приближённое решение нелинейного уравнения в пространстве непрерывных функций C[-1.5;1.5]');
 hold off;
 
-function max = maximum(x0, x1, a, b)
+function max = maximum(xn1, xn, a, b)
     max = 0.0;
     for i = a:1:b
-        value = abs(x0(i) - x1(i));
+        value = abs(xn1(i) - xn(i));
         if (value > max)
             max = value;
         end
--- grigoryev_16.m
+++ grigoryev_16.m
@@ -1,12 +1,8 @@
 % ПЗ 16, Григорьев Юрий
-clc;
+clc; syms t; syms s; syms f0(t); syms f(t); syms solution(t);
 alpha = 1/3;
 eps = 1e-5;
-syms t;
-syms s;
-syms f0(t);
 f0(t) = t;
-syms f(t);
 f(t) = (1/7) * int(f0(s) * (t+s)^2, s, 0, 1) - 2*t^2;
 apriori = int16(round(log(eps * (1 - alpha) / maximum(f0, f, 0, 1)) / log(alpha)) + 1);
 fprintf("lambda = 1/7, alpha = %f, eps = %.e, apriori = %d iterations\n", alpha, eps, apriori);
@@ -18,7 +14,6 @@ while i < apriori
 end
 f(t) % вывод приближённого решения
 
-syms solution(t);
 solution(t) = t^2 * (-2-16189/131083) - 23373*t/131083 - 17703/262166; % точное решение
 
 m = maximum(f, solution, 0, 1); % расстояние между приближённым и точным решением
@@ -30,10 +25,10 @@ fplot(f, "-");
 fplot(solution, "--");
 hold off;
 
-function max = maximum(x0, x1, a, b)
+function max = maximum(xn1, xn, a, b)
     max = 0.0;
     for i = a:1:b
-        value = abs(x0(i) - x1(i));
+        value = abs(xn1(i) - xn(i));
         if (value > max)
             max = value;
         end
